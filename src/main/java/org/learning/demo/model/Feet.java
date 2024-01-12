c3b432950a078e17c5c26e8034588009e2ff4bd2
pyroz7 <vincipyroz7@gmail.com>
2024-01-12 16:50:10 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/org/learning/demo/model/Feet.java
@@ -0,0 +1,18 @@
+package org.learning.demo.model;
+
+import java.math.BigDecimal;
+
+public class Feet {
+    public class Dress {
+        private String id;
+        private String beauty;
+        private BigDecimal price;
+
+        public Dress(String id, String color, BigDecimal price) {
+            this.id = id;
+            this.beauty = beauty;
+            this.price = price;
+        }
+    }
+}
+
