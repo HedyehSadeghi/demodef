64c423e8ff80cb3645b2f39dab244788cc202734
MatteoFarinazzo <matteofary@gmail.com>
2024-01-12 17:13:27 +0100

    mo te lo faccio vedere

--- src/main/java/org/learning/demo/Controller/ControllerDress.java
+++ src/main/java/org/learning/demo/Controller/ControllerDress.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Scanner;
+
 @Controller
 @RequestMapping
 public class ControllerDress {
     @GetMapping("/")
     public String home (Model model){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("ciao");
+
         return "index";
     }
 }
