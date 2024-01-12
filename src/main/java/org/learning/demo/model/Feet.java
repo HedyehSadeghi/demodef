9cedae53bbf4e77478ca8f956a1f179cf85ee616
pyroz7 <vincipyroz7@gmail.com>
2024-01-12 17:00:31 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/learning/demo/model/Feet.java
+++ src/main/java/org/learning/demo/model/Feet.java
@@ -3,12 +3,12 @@ package org.learning.demo.model;
 import java.math.BigDecimal;
 
 public class Feet {
-    public class Dress {
+    public static class Cap {
         private String id;
         private String beauty;
         private BigDecimal price;
 
-        public Dress(String id, String color, BigDecimal price) {
+        public  Cap (String id, String beauty, BigDecimal price) {
             this.id = id;
             this.beauty = beauty;
             this.price = price;
